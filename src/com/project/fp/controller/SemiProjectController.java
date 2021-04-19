a6df763a68a1861f7d057efacd16eda49b04e38a
qclass <rhkgkrwk1002@naver.com>
2021-04-19 19:13:06 +0900

    1

--- src/com/project/fp/controller/SemiProjectController.java
+++ src/com/project/fp/controller/SemiProjectController.java
@@ -60,12 +60,6 @@ public class SemiProjectController extends HttpServlet {
 		} else if (command.equals("doctor_signup")) {
 			response.sendRedirect("doctor_signup.jsp");
 		} else if (command.equals("signupres")) {
-			String animal_name = request.getParameter("animal_name");
-			String animal_gen = request.getParameter("animal_gen");
-			String animal_type = request.getParameter("animal_type");
-			int animal_age = Integer.parseInt(request.getParameter("animal_age"));
-			double animal_weight = Double.parseDouble(request.getParameter("animal_weight"));
-			String animal_unq = request.getParameter("animal_unq");
 			String member_id = request.getParameter("member_id");
 			String member_password = request.getParameter("member_password");
 			String member_name = request.getParameter("member_name");
@@ -77,18 +71,30 @@ public class SemiProjectController extends HttpServlet {
 			String member_animal = request.getParameter("member_animal");
 			String member_dr_info = request.getParameter("member_dr_info");
 			String member_notify = request.getParameter("member_notify");
-			AnimalDto a_dto = new AnimalDto();
-			a_dto.setAnimal_name(animal_name);
-			a_dto.setAnimal_gen(animal_gen);
-			a_dto.setAnimal_type(animal_type);
-			a_dto.setAnimal_age(animal_age);
-			a_dto.setAnimal_weight(animal_weight);
-			a_dto.setAnimal_unq(animal_unq);
-			a_dto.setMember_id(member_id);
+			
 			MemberDto m_dto = new MemberDto(member_id, member_password, member_name, member_nicname, member_email,
 					member_phone, member_addr, member_grade, "Y", member_animal, 0, member_dr_info, member_notify);
 			int m_res = m_biz.insert(m_dto);
-			int a_res = a_biz.insert(a_dto);
+			
+			int a_res = 0;
+			if (member_animal.equals("Y")) {
+				String animal_name = request.getParameter("animal_name");
+				String animal_gen = request.getParameter("animal_gen");
+				String animal_type = request.getParameter("animal_type");
+				int animal_age = Integer.parseInt(request.getParameter("animal_age"));
+				double animal_weight = Double.parseDouble(request.getParameter("animal_weight"));
+				String animal_unq = request.getParameter("animal_unq");	
+				AnimalDto a_dto = new AnimalDto();
+				a_dto.setAnimal_name(animal_name);
+				a_dto.setAnimal_gen(animal_gen);
+				a_dto.setAnimal_type(animal_type);
+				a_dto.setAnimal_age(animal_age);
+				a_dto.setAnimal_weight(animal_weight);
+				a_dto.setAnimal_unq(animal_unq);
+				a_dto.setMember_id(member_id);
+				a_res = a_biz.insert(a_dto);
+			}
+			
 			int res = m_res + a_res;
 			if (res > 0) {
 				jsResponse(response, "회원가입 성공", "index.html");
@@ -106,21 +112,6 @@ public class SemiProjectController extends HttpServlet {
 			MemberDto dto = m_biz.selectOne(m_dto);
 			HttpSession session = request.getSession();
 			session.setAttribute("dto", dto);
-			dispatch(response, request, "#.jsp");
-		}
-		
-		if (command.equals("login")) {
-			response.sendRedirect("login.jsp");
-		}
-		
-		if (command.equals("#")) {
-			String member_id = request.getParameter("member_id");
-			String member_password = request.getParameter("member_password");
-			MemberDto m_dto = null;
-			m_dto.setMember_id(member_id);
-			m_dto.setMember_password(member_password);
-			MemberDto dto = m_biz.selectOne(m_dto);
-			request.setAttribute("dto", dto);
 			dispatch(response, request, "#.jsp");
 		}
 		
